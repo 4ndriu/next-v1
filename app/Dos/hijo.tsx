937e7c87902f84a04cb82f672fee2287ada49faa
4ndriu <andres.castillop@uniagustiniana.edu.co>
2026-05-08 19:03:01 +0000

    solucion de errores tsx

--- app/Dos/hijo.tsx
+++ app/Dos/hijo.tsx
@@ -1,9 +1,13 @@
-export default function Hijo ({children}){
-    return(
-        <div>
-            este es mi hijo{children}
-            </div>
-        
-    )
-    
+import type { ReactNode } from 'react'
+
+type HijoProps = {
+  children?: ReactNode
+}
+
+export default function Hijo({ children }: HijoProps) {
+  return (
+    <div>
+      este es mi hijo{children}
+    </div>
+  )
 }
